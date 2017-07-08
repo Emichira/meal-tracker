dfe37a8a289674d6934b7d49267844cc1cc0c28e
EMichira <emmanuelmichira@gmail.com>
2017-07-08 16:39:32 +0300

    added data to view

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,12 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
-  <div class="container">
-    <h1>Meal Tracker</h1>
-  </div>
+    <div class="container">
+      <h1>Meal Tracker</h1>
+    </div>
   `
 })
-
 export class AppComponent {
-
 }
